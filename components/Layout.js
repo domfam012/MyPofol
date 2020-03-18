b8ec50fdc44d67acca4ad66a1ff591c2e9ee7c4f
bohyeonp <219031@naver.com>
2020-03-18 16:35:01 +0900

    myui-react

---
+++ components/Layout.js
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+import Header from './Header';
+import Footer from './Footer';
+import React from "react";
+
+const Layout = props => {
+    return (
+        <>
+            {/* 공통 head 영역 */}
+            <Head>
+                <title>무료 템플릿을 만나보세요. 부트스트랩 / 뷰 / 리액트 - My UI</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport"/>
+                <meta name="Referrer" content="origin"/>
+                <meta httpEquiv="X-UA-Compatible" content="IE=Edge"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+                {/* app 파비콘 설정 */}
+                <link rel="shortcut icon" type="image/x-icon" href="/img/common/favicon.ico"/>
+
+                <script src="/js/bundle.js"/>
+            </Head>
+            <div className={"wrap"}>
+                <Header/>
+                {props.children}
+                <Footer/>
+            </div>
+
+        </>
+    )
+};
+
+export default Layout;
---
+++ pages/_error.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+        요청하신 페이지를 찾을 수 없습니다.
+        <br />
+        삭제된 주소거나, 일시적으로 사용할 수 없습니다.
+    </div>
+  );
+};
+
+export default Page;

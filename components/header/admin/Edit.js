2398542ad14d5e944e4ea9fdc30b4ed5653c38d9
bohyeonp <219031@naver.com>
2020-03-20 18:33:51 +0900

    프로젝트 환경 세팅 컴포넌트 페이지

--- components/header/admin/Edit.js
+++ components/header/admin/Edit.js
@@ -1,32 +1,18 @@
-import React, {useState} from 'react';
-import {useRouter} from "next/router";
-import cookie from "js-cookie";
-
 // Header
 const Header = props => {
-    const router = useRouter();
-    const [menuActive, setMenuState] = useState(false);
-
-    //로그아웃
-    const logout = async (e) => {
-        e.preventDefault();
-        const check = confirm("로그아웃 하시겠습니까?");
-        cookie.remove('token');
-    }
-
     return (
-        <header className="header">
-            <div className="bg-header">
-                <nav className="navbar navbar-expand navbar-dark">
-                    <a className="navbar-brand" href="#"><img src="../img/common/logo.png" alt="MyUI"/></a>
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">Home<span className="sr-only">(current)</span></a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Documentation</a>
-                        </li>
-                    </ul>
+        <header className="bg-header">
+            <div className="container">
+                <nav className="navbar navbar-expand">
+                    <h1><a className="navbar-brand" href="#"><img src="/img/common/logo.png" alt="MyPofol"/></a></h1>
+                    <div className="collapse navbar-collapse">
+                        <ul className="navbar-nav mr-auto">
+                        </ul>
+                        <div className="login_area">
+                            <a className="login" href="#"><span className="text _name">로그인이 필요합니다.</span> <img
+                                src="/img/common/login.png" alt="myPofol"/></a>
+                        </div>
+                    </div>
                 </nav>
             </div>
         </header>

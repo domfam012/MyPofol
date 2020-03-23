a98fd7d7c34877ede555a3dc946a7367fde9bbb4
pyungwook-domfam <pyungwook.hur@domfam.com>
2020-03-23 16:15:34 +0900

    - site edit -> edit.js 추가 - component 로 분리 필요

--- components/Layout.js
+++ components/Layout.js
@@ -21,7 +21,7 @@ const Layout = props => {
                 <script src="/js/bundle.js"/>
             </Head>
             {/*m-wrap 분기처리 필요, 메인화면 UI 이슈로 임시 하드 코딩*/}
-            <div className={"wrap m-wrap"}>
+            <div className={"wrap"}>
                 {props.children}
                 <Footer/>
             </div>

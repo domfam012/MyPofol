2ddb3e69d8ebccb4fc231e979f9d61b5cdbb96db
pyungwook-domfam <pyungwook.hur@domfam.com>
2020-03-24 19:21:47 +0900

    - edit 컴포넌트 파일 내부 컴포넌트 분리 - edit 컴포넌트 파일명 변경 -> 뒤에 List 추가

--- pages/admin/edit.js
+++ pages/admin/edit.js
@@ -5,9 +5,9 @@ import Header from "../../components/header/admin/Edit";
 import { useRouter } from "next/router";
 
 // Site | Category | View 분기 처리 필요
-import Site from "../../components/pages/admin/edit/Site";
-import Category from "../../components/pages/admin/edit/Category";
-import View from "../../components/pages/admin/edit/View";
+import SiteList from "../../components/pages/admin/edit/SiteList";
+import CategoryList from "../../components/pages/admin/edit/CategoryList";
+import ViewList from "../../components/pages/admin/edit/ViewList";
 
 const Edit = props => {
   const router = useRouter();
@@ -20,16 +20,16 @@ const Edit = props => {
     // http://localhost/admin/edit?site=
     // http://localhost/admin/edit?category=
     // http://localhost/admin/edit?site=&category=
-    render = <Site/>;
+    render = <SiteList/>;
   } else if (site && category) {
     // http://localhost/admin/edit?site=ab&category=cd
     // -> View 가서 Site/Category 조회 후 없으면 404
-    render = <View/>;
+    render = <ViewList/>;
   } else if (site) {
     // http://localhost/admin/edit?site=ab
     // http://localhost/admin/edit?site=ab&category=
     // -> Category 가서 Site 조회 후 없으면 404
-    render = <Category/>;
+    render = <CategoryList/>;
   } else {
     // http://localhost/admin/edit?category=cd
     render = (<div>404</div>);

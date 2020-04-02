cadebece93d2adf1d4c85d5bb6483560334ff587
bohyeonp <219031@naver.com>
2020-04-02 17:07:47 +0900

    .

--- pages/admin/edit.js
+++ pages/admin/edit.js
@@ -8,6 +8,7 @@ import { useRouter } from "next/router";
 import Site from "../../components/pages/admin/edit/Site";
 import Category from "../../components/pages/admin/edit/Category";
 import View from "../../components/pages/admin/edit/View";
+import {PORTFOLIO_SITE_INFO} from "../../redux/reducers/user";
 
 const Edit = props => {
   const router = useRouter();
@@ -29,7 +30,7 @@ const Edit = props => {
     // http://localhost/admin/edit?site=ab
     // http://localhost/admin/edit?site=ab&category=
     // -> Category 가서 Site 조회 후 없으면 404
-    render = <Category/>;
+    render = <Category siteName={props.name}/>;
   } else {
     // http://localhost/admin/edit?category=cd
     render = (<div>404</div>);
@@ -41,6 +42,21 @@ const Edit = props => {
       {render}
     </Layout>
   );
+};
+
+
+Edit.getInitialProps = async function(ctx) {
+    console.log(ctx);
+    const site = ctx.query.site;
+    const name = ctx.query.name;
+    if(site){
+        console.log(site);
+        ctx.store.dispatch({
+            type : PORTFOLIO_SITE_INFO,
+            data : ctx.query.site
+        });
+        return ({siteName : name})
+    }
 };
 
 export default Edit;

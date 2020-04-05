90f0c69ac9e646a19dc07b7e885e343be3d2ef63
cheesepaninim <hpw0310@gmail.com>
2020-04-06 03:16:20 +0900

    prettier 적용 site 선택 정보 화면 노출 및 취소 기능 site 정보 조회

--- pages/api/user/[id]/site.js
+++ pages/api/user/[id]/site.js
@@ -6,58 +6,57 @@
 import { loadDB } from "../../../../public/js/db";
 
 export default async (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader("Content-Type", "application/json");
 
-    const { query: { id } } = req;
+  const {
+    query: { id }
+  } = req;
 
-    const db = await loadDB();
-    const doc = await db.collection("User").doc(id);
+  const db = await loadDB();
+  const doc = await db.collection("User").doc(id);
 
-    let resData;
+  let resData;
 
-    switch(req.method) {
+  switch (req.method) {
+    case "GET":
+      const ref = await doc.get();
+      resData = { site: [] };
 
-        case "GET":
-            const ref = await doc.get();
-            resData = {site: []};
+      // 사용자 사이트 목록
+      const siteList = ref.data().siteList;
 
-            // 사용자 사이트 목록
-            const siteList = ref.data().siteList;
-
-            // 사이트 collection 에서 해당 사이트들 조회
-            const getSub = async () => {
-
-                console.log(siteList)
-                for(let site of siteList) {
-                    const subDoc = await db.collection("Site").doc(site);
-                    const subRef = await subDoc.get();
-                    console.log(subRef.data())
-                    if (subRef._document !== null) {
-                        resData.site.push(subRef.data());
-                    }
-                    else {
-                        return res.status(500).send("user's sites not found")
-                    }
-                }
-            };
-            getSub();
+      // 사이트 collection 에서 해당 사이트들 조회
+      const getSub = async () => {
+        // console.log(siteList)
+        for (let site of siteList) {
+          const subDoc = await db.collection("Site").doc(site);
+          const subRef = await subDoc.get();
+          // console.log(subRef.data())
+          if (subRef._document !== null) {
+            resData.site.push(subRef.data());
 
             resData.status = 200;
             resData.msg = "success";
-            
-            // 정상 조회 응답 
+
+            // 정상 조회 응답
             return res.status(200).send(resData);
+          } else {
+            return res.status(500).send("user's sites not found");
+          }
+        }
+      };
+      getSub();
 
-        default: // GET 이외의 요청
-            resData = JSON.stringify({
-                status: 405,
-                msg: "not allowed method"
-            });
-            return res.status(405).send(resData);
-
-    }
-
+      break;
+    default:
+      // GET 이외의 요청
+      resData = JSON.stringify({
+        status: 405,
+        msg: "not allowed method"
+      });
+      return res.status(405).send(resData);
+  }
 };

e65022836098fcb101478581163f9ded486a1903
pyungwook-domfam <pyungwook.hur@domfam.com>
2020-04-07 15:29:20 +0900

    수정시 상태관리 안되던 버그 수정

--- components/popup/admin/new/Step2.js
+++ components/popup/admin/new/Step2.js
@@ -74,19 +74,16 @@ const Step2 = props => {
 
   return (
     <>
-      {openAlert ? (
-        <Alert message={msg} closeAlert={closeAlert} />
-      ) : (
-        <section className="container-fluid init info-pf">
-          <h2 className="sr-only">당신의 이름, 혹은 사이트명을 알려주세요.</h2>
-          <p className="title font-weight-normal pl mb">
-            <img src="/img/common/1.png" alt="1" />
-            당신의 <span className="font-weight-bold">이름,</span> 혹은{" "}
-            <span className="font-weight-bold">사이트명</span>을 알려주세요.
-          </p>
-          <form className="form_info mb">
-            <div className="form-group">
-              <input
+      <section className="container-fluid init info-pf">
+        <h2 className="sr-only">당신의 이름, 혹은 사이트명을 알려주세요.</h2>
+        <p className="title font-weight-normal pl mb">
+          <img src="/img/common/1.png" alt="1" />
+          당신의 <span className="font-weight-bold">이름,</span> 혹은{" "}
+          <span className="font-weight-bold">사이트명</span>을 알려주세요.
+        </p>
+        <form className="form_info mb">
+          <div className="form-group">
+            <input
                 id={"name"}
                 name={"name"}
                 value={name}
@@ -96,16 +93,16 @@ const Step2 = props => {
                 title="사이트명"
                 placeholder="사이트명"
                 style={{ width: "400px" }}
-              />
-              <span className="desc">
+            />
+            <span className="desc">
                 사이트 상단에 로고와 함께 표시되며, 언제든지 변경가능합니다.{" "}
               </span>
-            </div>
-            <div className="form-group">
+          </div>
+          <div className="form-group">
               <span className="domain d-inline-block">
                 http://www.mypofol.com/
               </span>
-              <input
+            <input
                 id={"site"}
                 name={"site"}
                 value={url}
@@ -115,19 +112,22 @@ const Step2 = props => {
                 title="사이트 주소*"
                 placeholder="사이트 주소*"
                 style={{ width: "200px" }}
-              />
-              <button className="btn btn-primary" onClick={checkUrl}>
-                사이트 중복확인
-              </button>
-            </div>
-          </form>
-          <div className="btn-area mb">
-            <button className="btn btn-xl btn-primary" onClick={handleNext}>
-              다음
+            />
+            <button className="btn btn-primary" onClick={checkUrl}>
+              사이트 중복확인
             </button>
           </div>
-        </section>
-      )}
+        </form>
+        <div className="btn-area mb">
+          <button className="btn btn-xl btn-primary" onClick={handleNext}>
+            다음
+          </button>
+        </div>
+        {openAlert ?
+            <Alert message={msg} closeAlert={closeAlert} /> : ''
+        }
+      </section>
+
     </>
   );
 };

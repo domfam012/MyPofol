e286123571d9961a3ef8ef6aee880512a4d0187a
pyungwook-domfam <pyungwook.hur@domfam.com>
2020-04-16 13:26:41 +0900

    사이트명 미입력 툴팁 regex -> url 체크 추가(영문, 숫자)

--- components/popup/admin/new/Step2.js
+++ components/popup/admin/new/Step2.js
@@ -18,26 +18,30 @@ const Step2 = props => {
   };
   const [msg, setMsg] = useState("");
 
-  const [nameFeed, setNameFeed] = useState("이름 혹은 사이트명을 입력해주세요.");
+  const [nameFeed, setNameFeed] = useState(
+    "이름 혹은 사이트명을 입력해주세요."
+  );
   const [openNameFeed, setOpenNameFeed] = useState(true);
   const [urlFeed, setUrlFeed] = useState("사이트주소를 입력해주세요.");
   const [openUrlFeed, setOpenUrlFeed] = useState(true);
-  const tooltipStyle = {
-    display: "block",
-    top: ""
-  };
 
   let isChecking = false;
 
   const onNameChange = e => {
     setName(e.target.value);
     handleNameChange(e.target.value);
+
+    if (e.target.value.length === 0) setOpenNameFeed(true);
+    else setOpenNameFeed(false);
   };
 
   const onUrlChange = e => {
     setUrl(e.target.value);
     setUrlChecked(false);
     handleUrlChange(e.target.value);
+
+    if (e.target.value.length === 0) setOpenUrlFeed(true);
+    else setOpenUrlFeed(false);
   };
 
   useEffect(() => {
@@ -68,8 +72,8 @@ const Step2 = props => {
     isChecking = true;
 
     // 이후 tooltip 으로 수정
-    if(!regex('eng').test(url)) {
-      setMsg("주소는 영문 대소문자만 입력 가능합니다.");
+    if (!regex("url").test(url)) {
+      setMsg("영어와 숫자만 입력 가능합니다.");
       setOpenAlert(true);
       return;
     }
@@ -78,7 +82,9 @@ const Step2 = props => {
       setMsg("사이트 url을 입력해주세요.");
       return setOpenAlert(true);
     }
-    const res = await axios.get(`${process.env.ASSET_PREFIX}/api/site/${url}/check`);
+    const res = await axios.get(
+      `${process.env.ASSET_PREFIX}/api/site/${url}/check`
+    );
     if (!res.data.urlChecked) {
       setMsg("이미 사용중인 주소입니다.");
       return setOpenAlert(true);
@@ -92,68 +98,86 @@ const Step2 = props => {
   };
 
   return (
-      <>
-        <section className="container-fluid init info-pf">
-          <h2 className="sr-only">당신의 이름, 혹은 사이트명을 알려주세요.</h2>
-          <p className="title font-weight-normal pl mb">
-            <img src="/img/common/1.png" alt="1" />
-            당신의 <span className="font-weight-bold">이름,</span> 혹은{" "}
-            <span className="font-weight-bold">사이트명</span>을 알려주세요.
-          </p>
-          <form className="form_info mb">
-            <div className="form-group">
-              <input
-                  id={"name"}
-                  name={"name"}
-                  value={name}
-                  onChange={onNameChange}
-                  type="text"
-                  className="form-control mb-1"
-                  title="사이트명*"
-                  placeholder="이름 혹은 사이트명을 입력하세요.*"
-                  maxLength={20}
-                  style={{ width: "400px" }}
-              />
+    <>
+      <section className="container-fluid init info-pf">
+        <h2 className="sr-only">당신의 이름, 혹은 사이트명을 알려주세요.</h2>
+        <p className="title font-weight-normal pl mb">
+          <img src="/img/common/1.png" alt="1" />
+          당신의 <span className="font-weight-bold">이름,</span> 혹은{" "}
+          <span className="font-weight-bold">사이트명</span>을 알려주세요.
+        </p>
+        <form className="form_info mb">
+          <div className="form-group">
+            <input
+              id={"name"}
+              name={"name"}
+              value={name}
+              onChange={onNameChange}
+              type="text"
+              className="form-control mb-1"
+              title="사이트명*"
+              placeholder="이름 혹은 사이트명을 입력하세요.*"
+              maxLength={20}
+              style={{ width: "400px" }}
+            />
+            {openNameFeed ? (
               <Tooltip
-                  feed={nameFeed}
-                  style={tooltipStyle}
+                feed={nameFeed}
+                style={{
+                  display: "block",
+                  top: "71%",
+                  width: "37%",
+                  left: "31.5%"
+                }}
               />
-              <span className="desc">
+            ) : (
+              ""
+            )}
+            <span className="desc">
               사이트 상단에 로고와 함께 표시되며, 언제든지 변경가능합니다.{" "}
             </span>
-            </div>
-            <div className="form-group">
+          </div>
+          <div className="form-group">
             <span className="domain d-inline-block">
               http://www.mypofol.com/
             </span>
-              <input
-                  id={"site"}
-                  name={"site"}
-                  value={url}
-                  onChange={onUrlChange}
-                  type="text"
-                  className="form-control d-inline-block ml-1"
-                  title="사이트 주소*"
-                  placeholder="사용할 도메인주소를 입력하세요.*"
-                  style={{ width: "200px" }}
-              />
+            <input
+              id={"site"}
+              name={"site"}
+              value={url}
+              onChange={onUrlChange}
+              type="text"
+              className="form-control d-inline-block ml-1"
+              title="사이트 주소*"
+              placeholder="사용할 사이트주소를 입력하세요.*"
+              style={{ width: "200px" }}
+            />
+            {openUrlFeed ? (
               <Tooltip
-                  feed={urlFeed}
-                  style={tooltipStyle}
+                feed={urlFeed}
+                style={{
+                  display: "block",
+                  top: "118%",
+                  width: "18%",
+                  left: "49.5%"
+                }}
               />
-              <button className="btn btn-primary" onClick={checkUrl}>
-                사이트 중복확인
-              </button>
-            </div>
-          </form>
-          <div className="btn-area mb">
-            <button className="btn btn-xl btn-primary" onClick={handleNext}>
-              다음
+            ) : (
+              ""
+            )}
+            <button className="btn btn-primary" onClick={checkUrl}>
+              사이트 중복확인
             </button>
           </div>
-          {openAlert ? <Alert message={msg} closeAlert={closeAlert} /> : ""}
-        </section>
-      </>
+        </form>
+        <div className="btn-area mb">
+          <button className="btn btn-xl btn-primary" onClick={handleNext}>
+            다음
+          </button>
+        </div>
+        {openAlert ? <Alert message={msg} closeAlert={closeAlert} /> : ""}
+      </section>
+    </>
   );
 };
 
--- public/js/regex.js
+++ public/js/regex.js
@@ -8,6 +8,7 @@
  */
 const regex = {
     eng: /^[a-zA-Z]+$/,
+    url: /^[a-zA-Z0-9]+$/,
     num: /^[0-9]+$/,
     email: /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i
 };
